efcbd48ddbfc198b9f5bd9233dc534e4eec38632
Sennett Lau <laub1199@gmail.com>
2023-10-04 13:44:35 +0800

    added solution 136

---
+++ solutions/136_single-number.go
@@ -0,0 +1,26 @@
+package solutions
+
+func SingleNumber(nums []int) int {
+	m := make(map[int]bool)
+	d := make(map[int]bool)
+
+	for i := 0; i < len(nums); i++ {
+		_, ok1 := m[nums[i]]
+		_, ok2 := d[nums[i]]
+
+		if ok2 {
+			continue
+		} else if ok1 {
+			delete(m, nums[i])
+			d[nums[i]] = true
+		} else {
+			m[nums[i]] = true
+		}
+	}
+
+	for k, _ := range m {
+		return k
+	}
+
+	return -1
+}
