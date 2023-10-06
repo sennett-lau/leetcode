91fcebafc0a08425fa327c3f923cd31f36795010
Sennett Lau <laub1199@gmail.com>
2023-10-07 02:08:28 +0800

    added solution 219

---
+++ solutions/219_contains-duplicate-ii.go
@@ -0,0 +1,14 @@
+package solutions
+
+func ContainsNearbyDuplicate(nums []int, k int) bool {
+	m := make(map[int]int)
+
+	for i, v := range nums {
+		if j, ok := m[v]; ok && i-j <= k {
+			return true
+		}
+		m[v] = i
+	}
+
+	return false
+}
